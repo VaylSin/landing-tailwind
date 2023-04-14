00db6932a3af8e2fd1c7b82547f259171f007a2d
VaylSin <slyb33developer@gmail.com>
2023-04-14 15:25:01 +0200

    premier commit post init

---
+++ src/components/Cardeal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cardeal = () => {
+  return (
+    <div>Cardeal</div>
+  )
+}
+
+export default Cardeal
---
+++ src/components/index.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./Navbar";
+import Business from "./Business";
+import Billing from "./Billing";
+import Cardeal from "./Cardeal";
+import Testimonials from "./Testimonials";
+import Clients from "./Clients";
+import Cta from "./Cta";
+import Footer from "./Footer";
+import Stats from "./Stats";
+import Hero from "./Hero";
+
+
+export {
+    Navbar,
+    Hero,
+    Business,
+    Billing,
+    Cardeal,
+    Testimonials,
+    Clients,
+    Cta,
+    Footer,
+    Stats,
+};
